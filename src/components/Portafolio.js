cb927a94776eb6c28b432e5288b01dc594511d22
clasesucatmarlon <clasesucatmarlon@gmail.com>
2021-03-16 21:49:24 -0500

    changed link for AppLinks

--- src/components/Portafolio.js
+++ src/components/Portafolio.js
@@ -35,7 +35,7 @@ const Portafolio = () => {
             imag: AppLinks,
             heading: 'Manage Links',
             textService: 'Application to manage links of interest, using Boostrap and Firebase as database management system.',
-            url: 'https://api-english-reactjs.web.app/'
+            url: 'https://applinks-cc96c.web.app/'
         },
     ]);
 
